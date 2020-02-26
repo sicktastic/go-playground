89c894537748356aa4ff7bdd163b7aa4f542d911
Anthony Lee <antwonlee@gmail.com>
2020-02-26 14:53:28 -0600

    Add pointer example *

--- main.go
+++ main.go
@@ -3,22 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	var i int
-	i = 42
-	fmt.Println(i)
+	// var i int
+	// i = 42
+	// fmt.Println(i)
 
-	var f float32 = 3.14
-	fmt.Println(f)
+	// var f float32 = 3.14
+	// fmt.Println(f)
 
-	firstName := "Anthony"
-	fmt.Println(firstName)
+	// firstName := "Anthony"
+	// fmt.Println(firstName)
 
-	b := true
-	fmt.Println(b)
+	// b := true
+	// fmt.Println(b)
 
-	c := complex(3, 4)
-	fmt.Println(c)
+	// c := complex(3, 4)
+	// fmt.Println(c)
 
-	r, im := real(c), imag(c)
-	fmt.Println(r, im)
+	// r, im := real(c), imag(c)
+	// fmt.Println(r, im)
+
+	var firstName *string = new(string)
+
+	*firstName = "Anthony"
+
+	fmt.Println(*firstName)
 }
