9ed8154e25da88abd80cedfadfd8594d4795b5c4
Anthony Lee <antwonlee@gmail.com>
2020-02-26 14:47:00 -0600

    Add variables

--- main.go
+++ main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	var i int
+	i = 42
+	fmt.Println(i)
+
+	var f float32 = 3.14
+	fmt.Println(f)
+
+	firstName := "Anthony"
+	fmt.Println(firstName)
 }
